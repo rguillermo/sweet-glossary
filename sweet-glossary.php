b57308951d936f6dc0af8f5236a46db6d96f54e4
rguillermo <jguillermo.dev@gmail.com>
2022-01-05 14:51:35 -0500

    :truck: chore: initialize plugin

---
+++ sweet-glossary.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Sweet Glossary
+ * Plugin URI:      https://techpecialist.com/sweet-glossary/
+ * Description:     A clean, simple and beautiful glossary plugin
+ * Author:          Guillermo
+ * Author URI:      https://techpecialist.com/
+ * Text Domain:     sweet-glossary
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Sweet_Glossary
+ */
+
+// Your code starts here.
